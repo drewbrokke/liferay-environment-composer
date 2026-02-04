78125ec6d91c373c5027823cb056551959188d6b
Drew Brokke <drew.brokke@liferay.com>
2026-02-03 18:19:02 -0600

    LPS-00000 FOO

--- scripts/export_liferay_logs.sh
+++ scripts/export_liferay_logs.sh
@@ -4,6 +4,6 @@ LIFERAY_EXPORTS_DIR="./exports/liferay"
 
 mkdir -p "${LIFERAY_EXPORTS_DIR}"
 
-docker compose cp liferay:/opt/liferay/logs "${LIFERAY_EXPORTS_DIR}" 2> /dev/null
-docker compose cp liferay:/opt/liferay/reports "${LIFERAY_EXPORTS_DIR}" 2> /dev/null
-docker compose cp liferay:/opt/liferay/routes "${LIFERAY_EXPORTS_DIR}" 2> /dev/null
+docker compose cp liferay:/opt/liferay/logs "${LIFERAY_EXPORTS_DIR}" 2>/dev/null
+docker compose cp liferay:/opt/liferay/reports "${LIFERAY_EXPORTS_DIR}" 2>/dev/null
+docker compose cp liferay:/opt/liferay/routes "${LIFERAY_EXPORTS_DIR}" 2>/dev/null
